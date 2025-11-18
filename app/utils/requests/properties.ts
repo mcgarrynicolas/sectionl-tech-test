42c70f26ce5fcf8d5ac24f5fe2633612c9dd5059
Lily (triple-crown) <tokikakerushojo@gmail.com>
2025-11-18 16:54:39 +0900

    fix: testing proxy service left in was a mistake

--- app/utils/requests/properties.ts
+++ app/utils/requests/properties.ts
@@ -3,7 +3,7 @@ import type { StrapiResponse, Property } from "../types/api";
 
 const API_URL = `/api/properties?populate=*`;
 async function getPropertiesFromApi(): Promise<StrapiResponse<Property[]>> {
-  const response = await axios.get(API_URL, { baseURL: import.meta.env.DEV ? "" : "https://proxy.corsfix.com/?https://content.section-l.co" });
+  const response = await axios.get(API_URL, { baseURL: import.meta.env.DEV ? "" : "https://content.section-l.co" });
   return response.data;
 }
 
